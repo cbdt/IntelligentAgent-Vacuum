d339883b6760c85ba47d49bbe51a8250ccbec8e8
Clément BAUDET <clement.baudet@etudiant.univ-rennes1.fr>
2020-02-06 16:42:45 -0500

    Add the wheels effector. It is used to move.

---
+++ src/agent/effectors/Wheels.java
@@ -0,0 +1,31 @@
+package agent.effectors;
+
+import agent.Agent;
+import environment.Environment;
+
+public class Wheels extends Effector{
+
+    public Wheels(Environment environment) {
+        super(environment);
+    }
+
+    @Override
+    public void makeAction(Agent.Action action) {
+        Environment.Position pos = new Environment.Position(0, 0);
+        switch (action) {
+            case MOVE_UP:
+                pos.setY(1);
+                break;
+            case MOVE_DOWN:
+                pos.setY(-1);
+                break;
+            case MOVE_LEFT:
+                pos.setX(-1);
+                break;
+            case MOVE_RIGHT:
+                pos.setX(1);
+                break;
+        }
+        getEnvionment().agentMovedSignal(pos);
+    }
+}
