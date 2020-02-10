178aac10ddb5a11670aa7964987e04822c7f1b7c
marque-antoine <dannerma@gmail.com>
2020-02-10 12:13:32 -0500

    env toujours

--- src/environment/Cell.java
+++ src/environment/Cell.java
@@ -1,5 +1,8 @@
 package environment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Cell {
     public enum State {
         EMPTY,
@@ -10,11 +13,71 @@ public class Cell {
 
     private State m_state;
 
+    public Environment.Position position;
+
     public State getState() {
         return m_state;
+    }
+
+    public Cell(int x, int y) {
+        x = 0;
+        y = 0;
+        this.m_state = State.EMPTY;
+        Environment.Position position = new Environment.Position(x, y);
+    }
+
+    public Cell(Environment.Position position, State state) {
+        state = State.EMPTY;
+        this.position = position;
+
+    }
+
+    public static boolean equalCells(Cell c1, Cell c2) {
+        return c1.position.x == c2.position.x && c1.position.y == c2.position.y;
+    }
+
+    public static boolean differentCells(Cell c1, Cell c2) {
+        return !equalCells(c1, c2);
+    }
+
+    public static Cell[] getCellPath(Cell start, Cell end) {
+
+    }
+
+    public void setM_state(State m_state) {
+        this.m_state = m_state;
+    }
+
+    public void setState(State state) {
+        this.m_state = state;
     }
 
     public Environment.Position getPosition() {
         return new Environment.Position(0, 0);
     }
+
+    public boolean isAbove(Cell cell) {
+        return position.y < cell.position.y;
+    }
+
+    public boolean isRightFrom(Cell cell) {
+        return position.x > cell.position.x;
+    }
+
+    public boolean isBelow(Cell cell) {
+        return position.y > cell.position.y;
+    }
+
+    public boolean isLeftFrom(Cell cell) {
+        return position.x < cell.position.x;
+    }
+
+    public <Cell> List<Cell> getNeighborCells(Cell cell, Cell[][] grid) {
+        List list = new ArrayList<Cell>();
+
+        if ()
+
+    }
+
+
 }
--- src/environment/Environment.java
+++ src/environment/Environment.java
@@ -1,9 +1,74 @@
 package environment;
+import java.util.Random;
 
 public class Environment implements Runnable {
-    //probabilité d'apparation de la poussiere et des bijoux
-    int dustSpawnProbability = 17;
-    int jewelDustProbability = 8;
+    //taille grille
+    public int minX = 0;
+    public int maxX = 4;
+    public int minY = 0;
+    public int maxY = 4;
+    //proba d'apparition
+    public int dustSpawnProb = 17;
+    public int jewelSpawnProb = 6;
+    //robot
+    public int performance;
+    public Position robotPosition = new Position(2, 2);
+    Random rand = new Random();
+    //Grille
+    private Cell[][] grid = new Cell[maxX + 1][maxY + 1];
+    private int updateTime = 500;
+
+    public void initGrid() {
+        for (int x = 0; x < maxX; x++) {
+            for (int y = 0; y < maxY; y++) {
+                grid[x][y] = new Cell(x, y);
+            }
+        }
+    }
+
+    public void generateDust() {
+        if (rand.nextInt(100) < dustSpawnProb) {
+            //generate(DUST)
+        }
+    }
+
+    public void generateJewel() {
+        if (rand.nextInt(100) < jewelSpawnProb) {
+            //generate(DUST)
+        }
+    }
+
+    // Fonction pour utiliser un thread, il faut que tu mette la boucle infinie dans cette fonction. Seulement cette fonction est executé (equivalent du main)
+    @Override
+    public void run() {
+        while (true) {
+
+        }
+    }
+
+    // FONCTIONS QUE J'UTILISE, si tu changes le nom, change aussi dans les classes pour l'agent
+    public Cell[][] getGrid() {
+        return new Cell[0][0];
+    }
+
+    // Lorsque l'agent bouge, je donne le delta du déplacement. (ex: il a effectué l'action de monté en haut, alors je te donne Position(0, 1), à gauche Position(-1, 0)...
+    public void agentMovedSignal(Position position) {
+
+    }
+
+    // Pour que le robot sache où il est au départ.
+    public Position getInitialPosition() {
+        return new Position(0, 0);
+    }
+
+    // Lorsque l'agent fait l'action de ramasser.
+    public void agentPickupSignal() {
+
+    }
+
+    // Lorsque l'agent aspire.
+    public void agentVacuumSignal() {
+    }
 
     public static class Position {
         int x;
@@ -30,27 +95,11 @@ public class Environment implements Runnable {
             this.y = y;
         }
 
-        public void add(Position position) {
+        public void update(Position position) {
             x += position.x;
             y += position.y;
         }
     }
 
-    // Fonction pour utiliser un thread, il faut que tu mette la boucle infinie dans cette fonction. Seulement cette fonction est executé (equivalent du main)
-    @Override
-    public void run() {
-
-    }
-
-    // FONCTIONS QUE J'UTILISE, si tu changes le nom, change aussi dans les classes pour l'agent
-    public Cell[][] getGrid() { return new Cell[0][0]; };
-    // Pour que le robot sache où il est au départ.
-    public Position getInitialPosition() { return new Position(0, 0); }
-    // Lorsque l'agent bouge, je donne le delta du déplacement. (ex: il a effectué l'action de monté en haut, alors je te donne Position(0, 1), à gauche Position(-1, 0)...
-    public void agentMovedSignal(Position position) {};
-    // Lorsque l'agent fait l'action de ramasser.
-    public void agentPickupSignal() {};
-    // Lorsque l'agent aspire.
-    public void agentVacuumSignal() {};
 
 }
